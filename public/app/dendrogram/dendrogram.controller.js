28a73f2d0ca5ba6da9447ed12ba91139f878b871
Justin Lawson <jaglawson3@gmail.com>
2016-02-15 11:11:29 -0700

    so far, stuff is working, need to finsih tooltip

--- public/app/dendrogram/dendrogram.controller.js
+++ public/app/dendrogram/dendrogram.controller.js
@@ -4,7 +4,7 @@
 
   function DendrogramController() {
     var vm = this
-    
+
     // Maybe do an $http to retrieve json data here,
     // instead of using D3.json. Not sure which is best.
     // Right now, .data is not doing anything, obvs.
