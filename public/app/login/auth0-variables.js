34ac06cb3a6027c0cf130b1c546c43f63bae08d7
Addison <addisonbennett@gmail.com>
2016-02-18 17:28:12 -0700

    removed login file

--- public/app/login/auth0-variables.js
+++
@@ -1,3 +0,0 @@
-var AUTH0_CLIENT_ID='qyC66gVCOylfeNJzYkLxsq1XTQc52mwe';
-var AUTH0_DOMAIN='treeoflife.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
--- public/app/login/userInfo.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.auth').controller('UserInfoCtrl', function(auth) {
-  // Using a promise
-  auth.profilePromise.then(function(profile) {
-    $scope.profile = profile;
-  });
-  // Or using the object
-  $scope.profile = auth.profile;
-});
